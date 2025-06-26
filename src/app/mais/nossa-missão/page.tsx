b0044a2306f6c5e59baa4192003dc8999ec2906f
marciotrindadefilho <marciotrindadefilho@icloud.com>
2025-06-26 14:45:02 -0300

    atualizando pasta src/app/mais/nossa-missão/page.tsx

--- src/app/mais/nossa-missão/page.tsx
+++ src/app/mais/nossa-missão/page.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 export default function Mission() {
   return (
@@ -38,7 +36,6 @@ export default function Mission() {
               </p>
             </div>
           </div>
-
         </div>
       </main>
 
